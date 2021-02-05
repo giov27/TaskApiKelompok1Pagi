fe56626106a7b13e6d760b6cf5313fac0f6eada6
abdur rohim <Abdur Rohim>
2021-02-05 15:29:08 +0700

    add PUT

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const { Router } = require("express")
 const express = require("express")
 const app = express()
-// const itemRoute = require('./route/')
+app.use(express.json())
+
+const rootRoute = require('./route/rootRoute')
+const itemRoute = require('./route/itemRoute')
+app.use(rootRoute, itemRoute)
 
 const port = 3000
 app.listen(port, () => {
     console.log(`server is listening on http://localhost:${port}`);
-})
+})  
--- route/itemRoute.js
+++ route/itemRoute.js
@@ -2,5 +2,21 @@ const express = require("express")
 const db = require("../db")
 const app = express.Router()
 
+// CRUD Item
+app.put('/item/:id', (req, res) => {
+    db[req.params.index] = req.body
+    res.send(req.body)
+})
 
+// CRUD Friend
+app.put('/friend/:id', (req, res) => {
+    db[req.params.index] = req.body
+    res.send(req.body)
+})
+
+// CRUD Transaction
+app.put('/Transaction/:id', (req, res) => {
+    db[req.params.index] = req.body
+    res.send(req.body)
+})
 module.exports = app
