ae22a54db82f228ffdb35feed0652868d5b608b6
mufidatunnisa <fidahani25@gmail.com>
2021-02-05 22:09:29 +0700

    error handler

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const express = require("express")
 const app = express()
 // const itemRoute = require('./route/')
 
+app.use(express.json())
+const itemRoute = require("./route/itemRoute")
+const friendRoute = require("./route/friendRoute")
+const transactionRoute = require("./route/transactionRoute")
+
+
+app.use(itemRoute, friendRoute, transactionRoute)
+
+app.use(function (eror, req, res, next) {
+    console.log(eror)
+    res.status(500).send(eror.message)
+})
+
 const port = 3000
 app.listen(port, () => {
     console.log(`server is listening on http://localhost:${port}`);
