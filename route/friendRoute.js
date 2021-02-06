e4ddf3fa9008113e6de8bcf20c1a296c102cd589
abdur rohim <Abdur Rohim>
2021-02-06 13:57:02 +0700

    add db

--- route/friendRoute.js
+++ route/friendRoute.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const db = require("../db")
+const db = require("../db/dbFriends")
 const app = express.Router()
 
 // CRUD Friend
--- route/transactionRoute.js
+++ route/transactionRoute.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const db = require("../db")
+const db = require("../db/dbTransactions")
 const app = express.Router()
 
 // CRUD Transaction
