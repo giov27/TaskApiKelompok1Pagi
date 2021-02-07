ec4538a3f631e0a18546064dab4bba2923c15eb1
alyachaerul <ralyachaerul@gmail.com>
2021-02-07 08:47:09 +0700

    register data to db and register handling

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+global.__rootdir = __dirname;
 const express = require("express")
 const app = express()
-
+const bodyParser = require('body-parser');
 const itemRoute = require('./route/itemRoute'); //import the route file
 
 // Define the middleware for the extended functionality in express
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 app.use(itemRoute);
 
--- route/itemRoute.js
+++ route/itemRoute.js
@@ -1,6 +1,8 @@
 const express = require("express")
-const db = require("../db")
 const app = express.Router()
+const fs = require('fs')
+const path = (`${__rootdir}/db.js`)
+console.log(__rootdir);
 
 // Get the User's Data in DB
 app.get('/', (req, res) => {
@@ -9,8 +11,49 @@ app.get('/', (req, res) => {
 
 // User Register Function
 app.post('/register', (req, res) => {
-    db.push(req.body);
-    res.send(req.body)
+    const {
+        id,
+        username,
+        password
+    } = req.body
+    let db;
+    try {
+        db = require(path);
+    } catch (err) {
+        db = fs.readFileSync(path);
+        db = db.replace('module.exports = ', '');
+        db = JSON.parse(db);
+    }
+    console.log(db);
+    if (db.length === 0) {
+        db.push({
+            id,
+            username,
+            password
+        })
+        console.log(db);
+        fs.writeFileSync(path, JSON.stringify(`module.exports = ${JSON.stringify(db)}`))
+        res.json({
+            'status': 'OK',
+            'description': 'Akun berhasil didaftarkan'
+        })
+    } else if (db.length > 0 && db.find(item => item.username === username)) {
+        res.json({
+            'status': 'WARNING',
+            'description': 'Username sudah terdaftar. Silakan gunakan username lain'
+        })
+    } else {
+        db.push({
+            id,
+            username,
+            password
+        });
+        fs.writeFileSync(path, JSON.stringify(`module.exports = ${JSON.stringify(db)}`));
+        res.json({
+            'status': 'OK',
+            'description': 'Username berhasil ditambahkan',
+        })
+    }
 });
 
 // User Login Function
