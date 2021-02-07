9ea263d9d68069ac8926456844af3f9617c747b2
giovmahe <giovannimaendra@gmail.com>
2021-02-07 17:27:11 +0700

    Update login success

--- route/registerRoute.js
+++ route/registerRoute.js
@@ -2,6 +2,7 @@ const express = require("express")
 const db = require("../dbRegister")
 const app = express.Router()
 const fs = require('fs')
+const { INSPECT_MAX_BYTES } = require("buffer")
 const path = (`${__rootdir}/db.js`)
 console.log(__rootdir);
 
@@ -58,22 +59,23 @@ app.post('/register', (req, res) => {
 });
 
 //check username is valid
-function validateLetter() {
-    var textInput = username;
-    var replacedInput = textInput.replace(/[^A-Za-z0-9]/g, "");
-    if (textInput != replacedInput) {
-        res.status(400).send("Invalid username! \n Only letter and numbers are allowed. No spaces.");
-    } else {
-        return username;
-    }
+// function validateLetter() {
+//     var textInput = username;
+//     var replacedInput = textInput.replace(/[^A-Za-z0-9]/g, "");
+//     if (textInput != replacedInput) {
+//         res.status(400).send("Invalid username! \n Only letter and numbers are allowed. No spaces.");
+//     } else {
+//         return username;
+//     }
 
 
-}
-console.log(validateLetter());
+// }
+// console.log(validateLetter());
 
 //Login feature
 app.post('/auth', (req, res) => {
     const {
+        id,
         username,
         password
     } = req.body
@@ -82,7 +84,7 @@ app.post('/auth', (req, res) => {
         const index = db.findIndex(item => item.username === username)
         // console.log(index);
         if (db[index].password === password) {
-            res.send(`Hello ${username}, you will be redirect to home`)
+            res.send(`Hello ${username}, \n Click to see: \n Items : http://localhost:3000/${db[index].id}/item \n Friends: http://localhost:3000/${db[index].id}/friend `)
         } else {
             res.status(400).send("Your password is incorrect")
         }
@@ -92,5 +94,7 @@ app.post('/auth', (req, res) => {
         res.status(400).send("Please input your username")
     }
 })
+
+app.get('/:id')
 
 module.exports = app
